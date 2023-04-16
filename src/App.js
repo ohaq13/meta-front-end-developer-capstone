a225f25dec1ddcdfcc518b160c1f4b7784e38ff3
ohaq13 <ohaq13@gmail.com>
2023-04-16 01:48:45 -0400

    Week2: Semantic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {
+  Route,
+  Routes
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+     <Header />
+       {/* <Routes> 
+     <Route path="/" exact element={<HomePage />}></Route>
+      <Route path="/booking" element={<BookingPage />}></Route>
+      <Route path="/confirmed" element={<ConfirmedBooking />}></Route>
+    </Routes>*/}
+     <Footer />
+
+    </>
   );
 }
 
